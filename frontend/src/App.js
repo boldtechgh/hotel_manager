efe14f325abdd164da8478640e6943999358c00a
devMozay <mosesanao13@gmail.com>
2022-12-05 21:24:05 +0000

    routing fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Login from './pages/Login/Login.jsx';
-import SignUp from './components/sign-up/sign-up.component';
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.scss";
+import Login from "./pages/Login/Login.jsx";
+import SignUp from "./components/sign-up/sign-up.component";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
-class App extends Component{
-
-  render(){
-    return(
-      <div className="App">
-        <Router>
-          <Routes>
-          <Route exact path="/" element={<Login></Login>} />
-            <Route path="/signup" element={<SignUp></SignUp>} />
-            </Routes>
-        </Router>
-    </div>
+class App extends Component {
+  render() {
+    return (
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     );
   }
 }
