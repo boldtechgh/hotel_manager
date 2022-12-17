73b5175c631033fff11c5aa6d39da236be81b548
Yahuza Nuhu <nuhuyahuza@yahoo.com>
2022-12-17 19:35:45 +0000

    header and check in changes

--- frontend/src/components/checkin-header/checkin-header.jsx
+++ frontend/src/components/checkin-header/checkin-header.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import Tabs from 'react-bootstrap/esm/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import CheckInDetails from '../checkin-details/checkin-details.component';
-
-
+import React from "react";
+import Tabs from "react-bootstrap/esm/Tabs";
+import Tab from "react-bootstrap/Tab";
+import CheckInDetails from "../checkin-details/checkin-details.component";
+import CustomerDetails from "../customer-details/customer-details.component";
 
 const CheckInHeader = () => {
-  
-    return(
-        <>
-        <div className='container'>
-            <Tabs
-      defaultActiveKey="checkin"
-      id="justify-tab-example"
-      className="mb-3"
-      justify
-    >
-      <Tab eventKey="home" title="Check In Details">
-      <CheckInDetails />
-      </Tab>
-      <Tab eventKey="profile" title="Customer Details">
-        Customer Details
-      </Tab>
-      <Tab eventKey="longer-tab" title="Billing Info">
-        Billing Info
-      </Tab>
-      <Tab eventKey="contact" title="Summary" >
-        Summary
-      </Tab>
-    </Tabs>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="container">
+        <Tabs
+          defaultActiveKey="checkin"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="home" title="Check In Details">
+            <CheckInDetails />
+          </Tab>
+          <Tab eventKey="profile" title="Customer Details">
+            <CustomerDetails />
+          </Tab>
+          <Tab eventKey="longer-tab" title="Billing Info">
+            Billing Info
+          </Tab>
+          <Tab eventKey="contact" title="Summary">
+            Summary
+          </Tab>
+        </Tabs>
+      </div>
+    </>
+  );
+};
 export default CheckInHeader;
--- frontend/src/components/customer-details/customer-details.component.jsx
+++ frontend/src/components/customer-details/customer-details.component.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Row, Col } from "react-bootstrap";
+import FormRange from "react-bootstrap/esm/FormRange";
 
 const CustomerDetails = () => {
-    return(
-        <>
-        <Form>
-            <Form.Group>
-                <Form.Label>Customer</Form.Label>
-            </Form.Group>
-        </Form>
-        </>
-    );
-}
+  return (
+    <>
+      <Form>
+        <Row>
+          <Col>
+            <Form.Control placeholder="First name" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Last name" />
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
 
 export default CustomerDetails;
